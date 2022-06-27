2f6184f2ba8fc6f9086861f6d3f6a04d5cae32d6
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-06-27 19:16:01 +0200

    contact component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import './contact.css';
+import "./contact.css";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  return (
-    <section id='contact'>contact</section>
-  )
-}
 
-export default Contact
+  const form = useRef(null);
+  const key = process.env.REACT_APP_EMAILJS_KEY
+
+  const sendEmail = (e:any) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_9r3ffmu', 'template_rxxn9vk', form.current!, `${key}`)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact__container">
+      <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your full name" required />
+          <input type="email" name="email" placeholder="Your eamil" required />
+          <textarea name="message" rows={7} placeholder="Your Message" required ></textarea>
+          <button type="submit" className="btn btn-primary">Send message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
