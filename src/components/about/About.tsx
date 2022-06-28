e4c0f730196ea621bb5abb6a035a2a30570356eb
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-06-28 22:54:53 +0200

    experience update

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./about.css";
 import ME from "../../assets/izabela2.jpg";
-import { FaAward } from "react-icons/fa";
+import { RiUser5Line } from "react-icons/ri";
+import { GiMountains } from "react-icons/gi";
+import { FaHandHoldingHeart } from "react-icons/fa";
 
-const about = () => {
+const About = () => {
+  const [aboutMeOpen, setAboutMeOpen] = useState(true);
+  const [hobbyOpen, setHobbyOpen] = useState(false);
+  const [valuesOpen, setValuesOpen] = useState(false);
+
+  const aboutMeHandler = () => {
+    setAboutMeOpen(true);
+    setHobbyOpen(false);
+    setValuesOpen(false)
+  };
+  const hobbyHandler = () => {
+    setHobbyOpen(true);
+    setAboutMeOpen(false);
+    setValuesOpen(false)
+  };
+  const valuesHandler = () => {
+    setValuesOpen(true)
+    setHobbyOpen(false);
+    setAboutMeOpen(false);
+  };
   return (
     <section id="about">
       <h5>Get to know me</h5>
@@ -12,44 +33,67 @@ const about = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-img">
-            <img src={ME} alt="meinnature"/>
+            <img src={ME} alt="me" />
           </div>
         </div>
 
         <div className="about__content">
           <div className="about__cards">
-            <article className="about__card">
-              <FaAward className="about__icon"/>
-              <h5>lorem ipsum</h5>
-              <small>lorem ipsum</small>
+            <article onClick={aboutMeHandler} className="about__card">
+              <RiUser5Line className="about__icon" />
+              <h5>Who am I? </h5>
             </article>
 
-            <article className="about__card">
-              <FaAward className="about__icon"/>
-              <h5>Lorem ipsum</h5>
-              <small>lorem ipsum</small>
+            <article onClick={hobbyHandler} className="about__card">
+              <GiMountains className="about__icon" />
+              <h5>Hobbies</h5>
             </article>
 
-            <article className="about__card">
-              <FaAward className="about__icon"/>
-              <h5>lorem ipsum</h5>
-              <small>lorem ipsum</small>
+            <article onClick={valuesHandler} className="about__card">
+              <FaHandHoldingHeart className="about__icon" />
+              <h5>Values</h5>
             </article>
           </div>
-          <p>
-            I'm open-minded and creative Full Stack JavaScript Developer from Poland
-            living in Oslo, Norway. I am a Finance and Accountancy graduate,
-            experienced in  e-commerce and sales in the insurance, clothing,
-            cosmetics industries and also in creating and running my own
-            business. Finally, I'm making my biggest dream come true by focusing
-            entirely on my passion - programming{" "}
-          </p>
-
-          <a href='#contact' className="btn btn-primary">Let's Talk!</a>
+          {aboutMeOpen && (
+            <p>
+              I'm open-minded and creative Full Stack JavaScript Developer from
+              Poland living in Oslo, Norway. Finance and Accountancy
+              graduate, experienced in e-commerce and sales in the insurance,
+              clothing, cosmetics industries and also in creating and running my
+              own business. Now, I am professionally fulfilling my passion -
+              programming. I have an analytical mind which I trained in economic
+              studies, but I also have sensitivity and artistic skills. I am
+              most fascinated by the frontend, UX and good design due to my
+              aesthetic sense{" "}
+            </p>
+          )}
+          {hobbyOpen && (
+            <p>
+              If you ask me where do I feel best? I will definitely answer among
+              nature, on the trail away from the noise of the city, surrounded
+              by forest and mountains. The love for Scandinavian landscapes made
+              me buy a one-way ticket and live in one of the most beautiful
+              countries - Norway. I love vanlife and I travel with my VW T4
+              California van at every free moment. I have an artistic soul,
+              sometimes I paint in my spare time. I am also passionate about
+              personal development and emotional inteligence.
+            </p>
+          )}
+          {valuesOpen && (
+            <p>
+              <li>* People and Cooperation</li>
+              <li>* Respect and Trust</li>
+              <li>* Passion and Engagement</li>
+              <li>* Creativity and Quality</li>
+            </p>
+          )}
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk!
+          </a>
         </div>
       </div>
     </section>
   );
 };
 
-export default about;
+export default About;
